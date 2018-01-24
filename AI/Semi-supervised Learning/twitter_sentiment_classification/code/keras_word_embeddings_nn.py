1b98f4a98221fe08b93bd1ebc4446c18c88251af
Ash Kent <ashley.kent@oneserve.co.uk>
2018-01-24 17:04:30 +0000

    Twitter sp.

--- AI/Semi-supervised Learning/twitter_sentiment_classification/code/keras_word_embeddings_nn.py
+++ AI/Semi-supervised Learning/twitter_sentiment_classification/code/keras_word_embeddings_nn.py
@@ -7,19 +7,25 @@ has been removed (class -1)
 
 """
 
-### load data
+### Load data
 import pandas as pd
 
 # there are 'full' datasets in the same location
 neg_datafile = '/Users/admin/Documents/Projects/MachineLearning/AI/Semi-supervised Learning/twitter_sentiment_classification/data/train_neg.txt'
 pos_datafile = '/Users/admin/Documents/Projects/MachineLearning/AI/Semi-supervised Learning/twitter_sentiment_classification/data/train_pos.txt'
 
-df_neg = pd.read_csv(neg_datafile, names=['tweet_str'], skiprows=0, delimiter='\n', error_bad_lines=False)
-print df_neg
-print df_neg.shape
+df_neg = pd.read_csv(neg_datafile, names=['tweet_str'], skiprows=0, delimiter='\n', error_bad_lines=False, encoding='latin-1')
+df_neg['target'] = -1
+print df_neg.shape	# (98954, 2)
 
-df_pos = pd.read_csv(pos_datafile, names=['tweet_str'], skiprows=0, delimiter='\n', error_bad_lines=False)
-print df_pos
-print df_pos.shape
+df_pos = pd.read_csv(pos_datafile, names=['tweet_str'], skiprows=0, delimiter='\n', error_bad_lines=False, encoding='latin-1')
+df_pos['target'] = 1
+print df_pos.shape	# (97718, 2)
 print df_pos.iloc[97692]	# creepy..
 
+df = pd.concat([df_neg, df_pos], ignore_index=True)
+print df.shape	# (196672, 2)
+
+
+
+### Preliminary cleaning of vocab
